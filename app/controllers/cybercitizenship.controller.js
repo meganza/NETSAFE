219f205e9d0bf2bf2d311fc72391f42f9765c8bb
meganza <meganza@gmail.com>
2017-02-22 22:02:40 +0800

    Help button - cybercitizenship
    Help button function for cybercitizenship page

--- app/controllers/cybercitizenship.controller.js
+++ app/controllers/cybercitizenship.controller.js
@@ -1,11 +1,7 @@
 angular.module('Netsafe').controller('cybercitizenshipController', function($scope) {
 	console.log('you are in cybercitizenshipController');
 
-	$scope.clicked = false;
-
-	$scope.clickHelp = function(){
-		$scope.clicked = !$scope.clicked;
-	}
+	$scope.includeTemplate = "/help/cybercitizenship.html"
 
 
 });
--- app/controllers/netsafe.controller.js
+++ app/controllers/netsafe.controller.js
@@ -1,9 +1,15 @@
 angular.module('Netsafe').controller('netsafeController', function($scope) {
 	console.log('you are in netsafe controller');
 
+	$scope.clicked = false;
+
 	$scope.toggleSidebar = function() {
 		$('#sidebar').toggle();
 	};
+
+	$scope.clickHelp = function(){
+		$scope.clicked = !$scope.clicked;
+	}
 
 
 	$scope.toggleSidebar();
