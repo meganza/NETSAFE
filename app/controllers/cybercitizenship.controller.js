62659f84ac1a3a83c6275460e89f336461728d08
meganza <meganza@gmail.com>
2017-02-22 23:01:26 +0800

    Help Button Function
    For all pages
    
    * Fix Fundamental Concepts dynamic pills

--- app/controllers/cybercitizenship.controller.js
+++ app/controllers/cybercitizenship.controller.js
@@ -1,7 +1,4 @@
 angular.module('Netsafe').controller('cybercitizenshipController', function($scope) {
 	console.log('you are in cybercitizenshipController');
 
-	$scope.includeTemplate = "/help/cybercitizenship.html"
-
-
 });
--- app/controllers/fundamental.controller.js
+++ app/controllers/fundamental.controller.js
@@ -1,4 +1,18 @@
 angular.module('Netsafe').controller('fundamentalController', function($scope) {
 	console.log('you are in fundamentalController');
 
+	$scope.ready = function(){
+		$('#conducting a').click(function(e){
+			e.preventDefault();
+			$(this).tab('show');
+		});
+	};
+
+	$scope.ready = function(){
+		$('#respecting a').click(function(e){
+			e.preventDefault();
+			$(this).tab('show');
+		});
+	};
+
 });
