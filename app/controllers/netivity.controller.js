977437bf4ad41e953c42f011a3b94b14d03e758e
meganza <meganza@gmail.com>
2017-04-15 23:50:51 +0800

    radio buttons, not showing

--- app/controllers/netivity.controller.js
+++ app/controllers/netivity.controller.js
@@ -39,47 +39,142 @@ angular.module('Netsafe').controller('netivityController', function($scope) {
     $(".button-num").removeClass("active");
     $(".scenario-content").hide();
 
-    $("#button-num-"+ id).addClass("active");
-    $("#scenario-content-" + id).show();
+    $("#hard-scenario-"+ id).addClass("active");
+    $("#hard-scenario-content-" + id).show();
   };
 
   $scope.showHardScenario(1);
-
 
   // Scenario contents
 
   $scope.easy1Scenarios = [
     {
       scenario: "What is needed in the scenario?",
-      answer: "Posting digital artworks of Thomas",
-      answered: false
+      answers: [
+        {
+          id: 1,
+          answer: "Posting digital artworks of Thomas"
+        },
+        {
+          id: 2,
+          answer: "---"
+        },
+        {
+          id: 3,
+          answer: "---"
+        },
+        {
+          id: 4,
+          answer: "---"
+        }
+      ]
     },
     {
       scenario: "Who are involved in the scenario?",
-      answer: "Thomas and an anonymous fellow",
-      answered: false
+      answers: [
+        {
+          id: 1,
+          answer: "---"
+        },
+        {
+          id: 2,
+          answer: "---"
+        },
+        {
+          id: 3,
+          answer: "Thomas and an anonymous fellow"
+        },
+        {
+          id: 4,
+          answer: "---"
+        }
+      ]
     },
     {
       scenario: "Who and what are affected by the scenario?",
-      answer: "Thomas",
-      answered: false
+      answers: [
+        {
+          id: 1,
+          answer: "---"
+        },
+        {
+          id: 2,
+          answer: "---"
+        },
+        {
+          id: 3,
+          answer: "Thomas"
+        },
+        {
+          id: 4,
+          answer: "---"
+        }
+      ]
     },
     {
       scenario: "What tools are used in the scenario?",
-      answer: "E-mail and Facebook",
-      answered: false
+      answers: [
+        {
+          id: 1,
+          answer: "---"
+        },
+        {
+          id: 2,
+          answer: "---"
+        },
+        {
+          id: 3,
+          answer: "---"
+        },
+        {
+          id: 4,
+          answer: "E-mail and Facebook"
+        }
+      ]
     },
     {
       scenario: "What are the actions of the people involved?",
-      answer: "Thomas posted his digital artworks on his facebook page.\
+      answers: [
+        {
+          id: 1,
+          answer: "Thomas posted his digital artworks on his facebook page.\
                 The anonymous fellow emailed Thomas that his digital artworks\
-                are trash and should quit creating digital arts.",
-      answered: false
+                are trash and should quit creating digital arts."
+        },
+        {
+          id: 2,
+          answer: "---"
+        },
+        {
+          id: 3,
+          answer: "---"
+        },
+        {
+          id: 4,
+          answer: "---"
+        }
+      ]
     },
     {
       scenario: "What guidelines are related to the scenario?",
-      answer: "Remember the Human",
-      answered: false
+      answers: [
+        {
+          id: 1,
+          answer: "---"
+        },
+        {
+          id: 2,
+          answer: "Remember the Human"
+        },
+        {
+          id: 3,
+          answer: "---"
+        },
+        {
+          id: 4,
+          answer: "---"
+        }
+      ]
     }
   ];
 
@@ -154,3 +249,5 @@ angular.module('Netsafe').controller('netivityController', function($scope) {
   ];
 
 });
+
+// if ($scope.answer1.id === 1) // this is the correct answer
