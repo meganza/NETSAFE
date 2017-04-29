30609519ffc0c87d4b39de99caf6505c6929f8ce
meganza <meganza@gmail.com>
2017-04-29 10:49:14 -0700

    bugs for netivity fix, ty gvc hehe

---
+++ app/models/scenarios.factory.js
@@ -0,0 +1,212 @@
+angular.module('Netsafe').factory('ScenariosFactory', function() {
+  return {
+    easy: [
+    {
+      scenario: "Sarah is online chatting with a 10-year-old boy. The boy sends Sarah a picture of " +
+                "himself and he wants Sara to send him a picture of herself and her family. Sara " +
+                "is thinking if she should send a picture or not. She decided to send a picture of " +
+                "herself and her family to the boy.",
+      answers: [
+        [
+          "---",
+          "---",
+          "To decide whether Sarah should send a picture or herself and her family or not.",
+          "---"
+        ],
+        [
+          "Sara and the 10-year-old boy",
+          "---",
+          "---",
+          "---"
+        ],
+        [
+          "---",
+          "---",
+          "---",
+          "Sarah, the boy, and Sarah’s family"
+        ],
+        [
+          "---",
+          "---",
+          "---",
+          "Online messaging application, family photo"
+        ],
+        [
+          "Sarah is chatting with a 10-year-old boy." +
+          "The boy sends Sarah a picture of himself." +
+          "The boy asks Sarah to send him a photo of herself and her family." +
+          "Sarah sends a picture of herself and her family to the boy.",
+          "---",
+          "---",
+          "---"
+        ],
+        [
+          "---",
+          "Giving away too much information (Digital Tattoo)",
+          "---",
+          "---"
+        ],
+        [
+          "---",
+          "---",
+          "---",
+          "correct"
+        ]
+      ]
+    },
+    {
+      scenario: "While you were in Middle School, you wrote a blog post where you criticized" +
+                "one of syour teachers for being mean to you in class. You claim the teacher" +
+                "was not fair in her grading and she insulted you in front of your classmates." +
+                "Further in the post, you ridicule the teacher’s age, race, and physical" +
+                "appearance. Now, you are in university and you are applying for a highly" +
+                "competitive internship. During the interview, the interviewer brings out a" +
+                "copy of the blog post and questions you about it.",
+
+      answer: {
+
+      }
+    }
+  ],
+
+  med: [
+    {
+      scenario: "medium is online chatting with a 10-year-old boy. The boy sends Sarah a picture of " +
+                "himself and he wants Sara to send him a picture of herself and her family. Sara " +
+                "is thinking if she should send a picture or not. She decided to send a picture of " +
+                "herself and her family to the boy.",
+      answers: [
+        [
+          "---",
+          "---",
+          "To decide whether Sarah should send a picture or herself and her family or not.",
+          "---"
+        ],
+        [
+          "Sara and the 10-year-old boy",
+          "---",
+          "---",
+          "---"
+        ],
+        [
+          "---",
+          "---",
+          "---",
+          "Sarah, the boy, and Sarah’s family"
+        ],
+        [
+          "---",
+          "---",
+          "---",
+          "Online messaging application, family photo"
+        ],
+        [
+          "Sarah is chatting with a 10-year-old boy." +
+          "The boy sends Sarah a picture of himself." +
+          "The boy asks Sarah to send him a photo of herself and her family." +
+          "Sarah sends a picture of herself and her family to the boy.",
+          "---",
+          "---",
+          "---"
+        ],
+        [
+          "---",
+          "Giving away too much information (Digital Tattoo)",
+          "---",
+          "---"
+        ],
+        [
+          "---",
+          "---",
+          "---",
+          "correct"
+        ]
+      ]
+    },
+    {
+      scenario: "While you were in Middle School, you wrote a blog post where you criticized" +
+                "one of syour teachers for being mean to you in class. You claim the teacher" +
+                "was not fair in her grading and she insulted you in front of your classmates." +
+                "Further in the post, you ridicule the teacher’s age, race, and physical" +
+                "appearance. Now, you are in university and you are applying for a highly" +
+                "competitive internship. During the interview, the interviewer brings out a" +
+                "copy of the blog post and questions you about it.",
+
+      answer: {
+
+      }
+    }
+  ],
+
+  hard: [
+    {
+      scenario: "hard is online chatting with a 10-year-old boy. The boy sends Sarah a picture of " +
+                "himself and he wants Sara to send him a picture of herself and her family. Sara " +
+                "is thinking if she should send a picture or not. She decided to send a picture of " +
+                "herself and her family to the boy.",
+      answers: [
+        [
+          "---",
+          "---",
+          "To decide whether Sarah should send a picture or herself and her family or not.",
+          "---"
+        ],
+        [
+          "Sara and the 10-year-old boy",
+          "---",
+          "---",
+          "---"
+        ],
+        [
+          "---",
+          "---",
+          "---",
+          "Sarah, the boy, and Sarah’s family"
+        ],
+        [
+          "---",
+          "---",
+          "---",
+          "Online messaging application, family photo"
+        ],
+        [
+          "Sarah is chatting with a 10-year-old boy." +
+          "The boy sends Sarah a picture of himself." +
+          "The boy asks Sarah to send him a photo of herself and her family." +
+          "Sarah sends a picture of herself and her family to the boy.",
+          "---",
+          "---",
+          "---"
+        ],
+        [
+          "---",
+          "Giving away too much information (Digital Tattoo)",
+          "---",
+          "---"
+        ],
+        [
+          "---",
+          "---",
+          "---",
+          "correct"
+        ]
+      ]
+    },
+    {
+      scenario: "While you were in Middle School, you wrote a blog post where you criticized" +
+                "one of syour teachers for being mean to you in class. You claim the teacher" +
+                "was not fair in her grading and she insulted you in front of your classmates." +
+                "Further in the post, you ridicule the teacher’s age, race, and physical" +
+                "appearance. Now, you are in university and you are applying for a highly" +
+                "competitive internship. During the interview, the interviewer brings out a" +
+                "copy of the blog post and questions you about it.",
+
+      answer: {
+
+      }
+    }
+  ]
+
+
+  }
+});
