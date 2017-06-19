d5547d8e3ddee8dc01d608402da6cd829337b670
meganza <meganza@gmail.com>
2017-06-19 15:28:30 +0800

    Finished 71% of UI Revisions

--- app/scripts/controllers/fundamental.controller.js
+++ app/scripts/controllers/fundamental.controller.js
@@ -1,6 +1,7 @@
 angular.module('Netsafe').controller('fundamentalController', function($scope) {
 	console.log('you are in fundamentalController');
 
+
 	$scope.showTab = function(id){
     $(".tab").removeClass("active");
     $(".tab-content").hide();
@@ -12,9 +13,15 @@ angular.module('Netsafe').controller('fundamentalController', function($scope) {
   $scope.showTab(1);
   $(".CYO-video").hide();
 
+  $("#CYO-video-1").show();
+  $("#cyo-btn-1").addClass("active");
+
   $scope.showCYOVid = function(id){
     $(".CYO-video").hide();
     $("#CYO-video-" + id).show();
+
+    $(".cyo-btn").removeClass("active");
+    $("#cyo-btn-" + id).addClass("active");
   };
 
 });
