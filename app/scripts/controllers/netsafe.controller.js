197e858517c98045c56c014495b329d7d63d97c7
meganza <meganza@gmail.com>
2017-06-22 20:26:59 +0800

    Added tooltip icon & function

--- app/scripts/controllers/netsafe.controller.js
+++ app/scripts/controllers/netsafe.controller.js
@@ -2,10 +2,15 @@ angular.module('Netsafe').controller('netsafeController', function($scope) {
 	console.log('you are in netsafe controller');
 
 	$scope.clicked = false;
+  $scope.clickedTool = false;
 
 	$scope.toggleSidebar = function() {
 		$('#sidebar').toggle();
 	};
+
+  $scope.clickToolTip = function(){
+    $scope.clickedTool = !$scope.clickedTool;
+  };
 
 	$scope.clickHelp = function(){
 		$scope.clicked = !$scope.clicked;
