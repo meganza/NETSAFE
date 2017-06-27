789fedbaea32ded08c3e25768bd9ddfe10bea52a
meganza <meganza@gmail.com>
2017-06-27 22:36:17 +0800

    Fixed overlapping videos on headers?

--- app/scripts/controllers/cybercitizenship.controller.js
+++ app/scripts/controllers/cybercitizenship.controller.js
@@ -9,13 +9,27 @@ angular.module('Netsafe').controller('cybercitizenshipController', function($sco
   $scope.cyberCitizenshipVid = 'https://www.youtube.com/watch?v=Xp4tWIy5rnc';
 
   // how to target a single video ?
-  // keeps pausing
   $scope.$on('youtube.player.playing', function($event, player){
-    setTimeout($scope.pauseVideo = function(){
-                player.pauseVideo();
-              }, 3000);
+    // pauses AFTER 3 SECONDS after EACH play/pause
+
     // setTimeout($scope.pauseVideo = function(){
     //             player.pauseVideo();
-    //           }, 6000);
+    //           }, 3000);
+
+    // var time = player.getCurrentTime();
+    // console.log(time);
+
+    // if(time < 4500){
+    //   var rate = player.getPlaybackRate();
+    //   var remainingTime = (4500 - time) / rate;
+    //   setTimeout($scope.pauseVideo = function(){
+    //             player.pauseVideo();
+    //           }, remainingTime * 1000);
+    // }
+
+
   });
+
+
+
 });
