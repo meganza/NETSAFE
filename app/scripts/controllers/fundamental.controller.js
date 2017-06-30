a2882b7919341d81eb59cfe80f71a1c503d366db
meganza <meganza@gmail.com>
2017-06-30 14:45:02 +0800

    Targeting specific playing videos

--- app/scripts/controllers/fundamental.controller.js
+++ app/scripts/controllers/fundamental.controller.js
@@ -10,6 +10,48 @@ angular.module('Netsafe').controller('fundamentalController', function($scope) {
   $scope.internetPrivacyVid = 'https://www.youtube.com/watch?v=AsidPDo6icM';
   $scope.respectingOthersVid = 'https://www.youtube.com/watch?v=H0TQIJdJw_U';
 
+  var pauseCount = 0;
+
+  $scope.$on('youtube.player.playing', function($event, player){
+    console.log(player.getVideoData().video_id); // get the video id of the playing video
+
+    if(player.getVideoData().video_id == "lljOiBv4V6w"){
+      if (pauseCount == 0){
+        // pause at first pause time
+        setTimeout($scope.pauseVideo = function(){
+                    player.pauseVideo();
+                  }, 3000);   // pauses at 0:02 [0, 1, 2]
+        pauseCount++;
+      } else if (pauseCount == 1){
+        setTimeout($scope.pauseVideo = function(){
+                    player.pauseVideo();
+                  }, 6000);   // pauses at 0:08 [3, 4, 5, 6, 7, 8]
+        // second pause time
+        pauseCount++;
+      } else if (pauseCount == 2){
+        // third pause count
+        setTimeout($scope.pauseVideo = function(){
+                    player.pauseVideo();
+                  }, 2000);   // pauses at 0:08 [9, 10]
+        // if last,
+        pauseCount = 10;
+      }
+    } else if (player.getVideoData().video_id == "AsidPDo6icM"){
+      console.log("hello, internet privacy vid is playing");
+    } else {
+      console.log("hello, respecting others  vid is playing");
+    }
+
+    // problem: its not at a SPECIFIC time, pause @ ___ seconds, play, pauses @ ___ seconds
+    // if the pause is by the teacher, then it will work up everything
+    console.log(pauseCount);
+  });
+
+  // puts pauseCount counter back to 0
+  $scope.$on('youtube.player.ended', function($event, player){
+    pauseCount = 0;
+    console.log(pauseCount);
+  });
 
 	$scope.showTab = function(id){
     $(".tab").removeClass("active");
--- app/scripts/controllers/techniques.controller.js
+++ app/scripts/controllers/techniques.controller.js
@@ -12,4 +12,32 @@ angular.module('Netsafe').controller('techniquesController', function($scope) {
   $scope.pharmingVid = 'https://www.youtube.com/watch?v=S3-GvYfb2cI';
   $scope.rogueVid = 'https://www.youtube.com/watch?v=E4BaYDaJiJ0';
 
+  var pauseCount = 0;
+
+  $scope.$on('youtube.player.playing', function($event, player){
+    console.log(player.getVideoData().video_id); // get the video id of the playing video
+
+    if(player.getVideoData().video_id == "qguDRN2jxGs"){
+      console.log("hello, identity theft vid is playing");
+    } else if (player.getVideoData().video_id == "tqTP-CI0yLM"){
+      console.log("hello, spam vid is playing");
+    } else if (player.getVideoData().video_id == "Dqcv_fHhuCY"){
+      console.log("hello, phishing vid is playing");
+    } else if (player.getVideoData().video_id == "S3-GvYfb2cI"){
+      console.log("hello, pharming vid is playing");
+    } else {
+      console.log("hello, rogue vid is playing");
+    }
+
+    // problem: its not at a SPECIFIC time, pause @ ___ seconds, play, pauses @ ___ seconds
+    // if the pause is by the teacher, then it will work up everything
+    console.log(pauseCount);
+  });
+
+  // puts pauseCount counter back to 0
+  $scope.$on('youtube.player.ended', function($event, player){
+    pauseCount = 0;
+    console.log(pauseCount);
+  });
+
 });
